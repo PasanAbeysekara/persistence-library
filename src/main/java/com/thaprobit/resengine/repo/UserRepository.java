3828e9c66a5b433019e57ca17738764da462a2b2
Chanmini <kavinyachanmini@gmail.com>
2024-03-11 05:51:31 +0530

    User registration

--- src/main/java/com/thaprobit/resengine/repo/UserRepository.java
+++ src/main/java/com/thaprobit/resengine/repo/UserRepository.java
@@ -2,9 +2,13 @@ package com.thaprobit.resengine.repo;
 
 import com.thaprobit.resengine.dao.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+   // @Query("SELECT MAX(u.user_id) FROM users u")
+   // Optional<Long> findMaxUserId();
 }
