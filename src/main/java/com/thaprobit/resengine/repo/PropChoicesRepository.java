fe6fcad35b3b2a80ce3b807ef77c58f7f423de83
Pasan Abeysekara <pasankavindaabey@gmail.com>
2024-04-15 21:09:28 +0530

    Add PropChoiceRepository for ease of fetching Restaurant Food Choices

--- src/main/java/com/thaprobit/resengine/repo/PropChoicesRepository.java
+++ src/main/java/com/thaprobit/resengine/repo/PropChoicesRepository.java
@@ -1,18 +1,28 @@
 package com.thaprobit.resengine.repo;
 
 import com.thaprobit.resengine.dao.PropChoices;
+import com.thaprobit.resengine.dao.key.PropChoiceID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Tharindu Aththanayake
  */
-public interface PropChoicesRepository extends JpaRepository<PropChoices, Integer>
+public interface PropChoicesRepository extends JpaRepository<PropChoices, PropChoiceID>
 {
 	@Query(value = "SELECT prop_id FROM {h-schema}prop_choices where choice_id =:choiceId", nativeQuery = true)
 	List<Long> findPropertiesForChoice( @Param("choiceId") Integer choiceId );
+
+	@Query(value = "SELECT choice_id FROM {h-schema}prop_choices WHERE prop_id = :propId", nativeQuery = true)
+	List<Integer> findChoicesByPropertyId(@Param("propId") Long propId);
+
+	@Query(value = "SELECT * FROM {h-schema}prop_choices WHERE prop_id = :propId", nativeQuery = true)
+	List<PropChoices> findByPropertyId(@Param("propId") Long propId);
+
+	Optional<PropChoices> findByPropChoiceId(PropChoiceID propChoiceId);
 
 }
